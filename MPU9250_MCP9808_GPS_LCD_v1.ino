7e1f204f4d0106859f45fffae4e3a353d2ee1630
Ivica Janekovic <ivica.jan@gmail.com>
2022-03-29 20:26:28 +0800

    Update MPU9250_MCP9808_GPS_LCD_v1.ino
    cleanup in devel branch

--- MPU9250_MCP9808_GPS_LCD_v1.ino
+++ MPU9250_MCP9808_GPS_LCD_v1.ino
@@ -5,24 +5,37 @@ MCP 9808 Temp
 GPS
 ***************************************************************************/
 #include <Wire.h>
+#define USE_MPU
+#define USE_MCP
+#define USE_GPS
+#define USE_LCD
+#define USE_WEB
+
+#ifdef USE_MPU
 // IMU9250
 #include <MPU9250_WE.h>
 #define MPU9250_ADDR 0x68
 #define SDA_PIN 23
 #define SCL_PIN 22
 MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
+#endif
 
+#ifdef USE_MCP
 // MCP9808 Temp sensor
 #include "Adafruit_MCP9808.h"
 #define MCP9808_ADDR 0x18
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();  // Create the MCP9808 temperature sensor object
+#endif
 
+#ifdef USE_LCD
 // LCD part, I2C address is 0x3C, connected to 19 and 18 pin
 #define OLED_SDA 19
 #define OLED_SCL 18
 #include <U8g2lib.h>
 U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ OLED_SCL, /* data=*/ OLED_SDA, /* reset=*/ U8X8_PIN_NONE);
+#endif
 
+#ifdef USE_GPS
 // GPS
 #include <TinyGPS++.h>
 // GPS setup GPIO 16 -> GPS RX , GPIO 17 -> GPS TX
@@ -30,18 +43,20 @@ static const int RXPin = 17, TXPin = 16;
 static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
 HardwareSerial ss(1);
+#endif
 
+#ifdef USE_WEB
 // WEB SERVER
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 const char* ssid = "ESP32";   // Wifi ssid and password
-const char* password = "tt";
+// const char* password = "tt";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 // Create an Event Source on /events
 AsyncEventSource events("/events");
-
+#endif
 
 #include <Time.h>
 const int time_offset = 8*3600;  // Local Time (AWST)
@@ -61,7 +76,7 @@ int readingID = 0;
 
 const float Pi = 3.14159;
 const float Declination = 1.0; // Magnetic declination for Perth
-float heading, heading_m, pitch, roll, temperature, MPU_temp, resultantG ;
+float heading, MPU_heading, pitch, roll, temperature, MPU_temp, resultantG ;
 
 String processor(const String& var){
 //  getSensorReadings();
@@ -71,6 +86,9 @@ String processor(const String& var){
   }
   else if(var == "HEADING"){
     return String(heading);
+  }
+  else if(var == "ROLL"){
+    return String(roll);
   }
   else if(var == "PITCH"){
     return String(pitch);
@@ -97,26 +115,28 @@ const char index_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
   <div class="topnav">
-    <h1>IMU 9250 </h1>
+    <h1>IMU MCP GPS </h1>
   </div>
   <div class="content">
     <div class="cards">
-      <div class="card">
-        <p><i class="fas fa-thermometer-half" style="color:#059e8a;"></i> TEMPERATURE</p><p><span class="reading"><span id="temp">%TEMPERATURE%</span> &deg;C</span></p>
-      </div>
       <div class="card">
         <p><i class="fas fa-tint" style="color:#00add6;"></i> HEADING</p><p><span class="reading"><span id="hum">%HEADING%</span> &deg N;</span></p>
       </div>
       <div class="card">
         <p><i class="fas fa-angle-double-down" style="color:#e1e437;"></i> PITCH</p><p><span class="reading"><span id="pitch">%PITCH%</span> &deg; </span></p>
       </div>
+      <div class="card">
+        <p><i class="fas fa-angle-double-down" style="color:#e1e437;"></i> ROLL</p><p><span class="reading"><span id="roll">%ROLL%</span> &deg; </span></p>
+      </div>
+      <div class="card">
+        <p><i class="fas fa-thermometer-half" style="color:#059e8a;"></i> TEMPERATURE</p><p><span class="reading"><span id="temperature">%TEMPERATURE%</span> &deg;C</span></p>
+      </div>
     </div>
   </div>
 <script>
 
 if (!!window.EventSource) {
- var source = new EventSource('/events');
- 
+ var source = new EventSource('/events'); 
  source.addEventListener('open', function(e) {
   console.log("Events Connected");
  }, false);
@@ -144,101 +164,40 @@ if (!!window.EventSource) {
   console.log("pitch", e.data);
   document.getElementById("pitch").innerHTML = e.data;
  }, false);
+
+ source.addEventListener('roll', function(e) {
+  console.log("roll", e.data);
+  document.getElementById("roll").innerHTML = e.data;
+ }, false);
 }
+
 </script>
 </body>
 </html>)rawliteral";
  
 
 void setup() {
- 
   int cnt = 0;
   Serial.begin(115200);
- 
-  // WIFI server
-  WiFi.softAP(ssid);
-  Serial.println(WiFi.softAPIP());    // Print ESP32 Local IP Address
+
+  init_WIFI();
   delay(100);
-    
-  // LCD INIT
-  u8g2.begin();     // Start display library
-  u8g2.clearBuffer();
-  u8g2_prepare(); 
-  u8g2.drawBox(5,10,20,10);
-  u8g2.sendBuffer();
+  
+  init_LCD();
   delay(50);
 
-  //MPU 9250
-  Wire.begin(SDA_PIN, SCL_PIN);
-  if(!myMPU9250.init()){
-    Serial.println("MPU9250 does not respond");
-  }
-  else{
-    Serial.println("MPU9250 is connected");
-  }
- if(!myMPU9250.initMagnetometer()){
-    Serial.println("Magnetometer does not respond");
-  }
-  else{
-    Serial.println("Magnetometer is connected");
-  }
+  init_MPU();
   delay(50);  
 
-  // Calibrate MPU 
-  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
-  delay(2000);
-  myMPU9250.autoOffsets();
-  Serial.println("Offset done!");
-  myMPU9250.enableGyrDLPF();
-  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
-  myMPU9250.setSampleRateDivider(5);
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
-  myMPU9250.enableAccDLPF(true);
-  myMPU9250.setAccDLPF(MPU9250_DLPF_6);  
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
-  delay(50);  
-
-  // MCP 9808 init
-  while (!tempsensor.begin(MCP9808_ADDR)) {
-    Serial.println("Couldn't find MCP9808!");
-    cnt++;
-    if (cnt > 10) {
-      return;
-    }
-    delay(50);
-  }
-  // tempsensor.shutdown_wake(0);
-  // delay(250);
-  tempsensor.setResolution(3);  // 0.0625°C    250 ms
-  float c = tempsensor.readTempC();
-  Serial.println(" MCP9808 temp: " + String(c));
-  Serial.println(" ");
+  init_MCP();
   delay(50);  
   
-  // GPS init
-  ss.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin); 
-
-  // WEB server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // Handle Web Server Events
-  events.onConnect([](AsyncEventSourceClient *client){
-    if(client->lastId()){
-      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
-    }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
-    client->send("hello!", NULL, millis(), 10000);
-  });
-  server.addHandler(&events);
-  server.begin();
+  init_GPS();
+  delay(50);
   
+  init_WEB();
   delay(200);  
 }
-
 
 void loop() {    
 
@@ -276,7 +235,6 @@ void loop() {
   Serial.print(pitch); 
   Serial.print("  |  Roll    = "); 
   Serial.println(roll); 
-
 /*
   Serial.println("Magnetometer Data in µTesla: ");
   Serial.print(magValue.x);
@@ -284,46 +242,42 @@ void loop() {
   Serial.print(magValue.y);
   Serial.print("   ");
   Serial.println(magValue.z);
- */
-  
-  Serial.print("Heading: ");
-  heading_m = atan2(magValue.x, magValue.y) * 180 / Pi;
-  Serial.print(heading_m); 
-  Serial.print("    |    Heading Corr: ");
+ */  
+  Serial.print("HEADING  MPU:");
+  MPU_heading = atan2(magValue.x, magValue.y) * 180 / Pi;
+  Serial.print(MPU_heading); 
+  Serial.print("    |    COR: ");
   heading = atan2(Xh, Yh) * 180 / Pi;
   heading += Declination ; 
-  
   if (heading > 360.0) heading -= 360.0;
   if (heading < 0.0)   heading += 360.0;
   // ----- Allow for under/overflow
   if (heading < 0) heading += 360 ;
   if (heading >= 360) heading -= 360 ;
-
   Serial.println(heading); 
   
+  // MCP TEMPERATURE
   float temperature = tempsensor.readTempC();
-  Serial.print(" MCP9808 temp: " + String(temperature));
-  Serial.println("   |    MPU9250 temp: " + String(MPU_temp));
-
+  Serial.print("TEMPERATURE MCP: " + String(temperature));
+  Serial.println("   |    MPU: " + String(MPU_temp));
 /*
   Serial.print("Orientation of the module: ");
   Serial.println(myMPU9250.getOrientationAsString());
 */
 
-  // GET GPS DATA
+  // GPS DATA
   SerialGPSDecode(ss, gps);
   
   // DISPLAY DATA ON LCD 
   Serial.println("********************************************");
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_6x10_tf);
-  //u8g2.setFont(u8g2_font_helvB10_tr); 
   u8g2.setCursor(0, 0);
   u8g2.print("HEADING ");
   u8g2.setCursor(50, 0);
   u8g2.print(String(heading,1));
   u8g2.setCursor(90, 0);
-  u8g2.print(String(heading_m,1));
+  u8g2.print(String(MPU_heading,1));
   u8g2.setCursor(0, 10);
   u8g2.print("PITCH ");
   u8g2.setCursor(35, 10);
@@ -351,7 +305,6 @@ void loop() {
   u8g2.setCursor(0, 40);
   u8g2.print("Temp (C): ");
   u8g2.setCursor(70, 40);
-  // u8g2.print(String(temp,1)); // MPU temp
   u8g2.print(String(temperature,1));   // MCP 9808 temp
   u8g2.setCursor(0, 50);
   u8g2.print("Total  G:");
@@ -365,12 +318,104 @@ void loop() {
   events.send("ping",NULL,millis());
   events.send(String(temperature).c_str(),"temperature",millis());
   events.send(String(heading).c_str(),"heading",millis());
+  events.send(String(roll).c_str(),"roll",millis());
   events.send(String(pitch).c_str(),"pitch",millis()); 
   delay(50);
 }
 
-// functions
-void u8g2_ascii_2() {
+// DEFINITION OF USED FUNCTIONS 
+
+void init_WIFI(void) {
+  // WIFI server in AP mode
+  WiFi.softAP(ssid);
+  Serial.println(WiFi.softAPIP());
+}    
+
+void init_LCD(void) {
+  // LCD INIT
+  u8g2.begin();     // Start display library
+  u8g2.clearBuffer();
+  u8g2_prepare(); 
+  u8g2.drawBox(5,10,20,10);
+  u8g2.sendBuffer();
+}
+
+void ini_MPU(void) {
+  //MPU 9250
+  Wire.begin(SDA_PIN, SCL_PIN);
+  if(!myMPU9250.init()){
+    Serial.println("MPU9250 does not respond");
+  }
+  else{
+    Serial.println("MPU9250 is connected");
+  }
+ if(!myMPU9250.initMagnetometer()){
+    Serial.println("Magnetometer does not respond");
+  }
+  else{
+    Serial.println("Magnetometer is connected");
+  }
+  delay(50);  
+
+  // Calibrate MPU 
+  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
+  delay(2000);
+  myMPU9250.autoOffsets();
+  Serial.println("Offset done!");
+  myMPU9250.enableGyrDLPF();
+  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
+  myMPU9250.setSampleRateDivider(5);
+  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
+  myMPU9250.enableAccDLPF(true);
+  myMPU9250.setAccDLPF(MPU9250_DLPF_6);  
+  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+}    
+
+void init_MCP(void) {
+  // MCP 9808 init
+  while (!tempsensor.begin(MCP9808_ADDR)) {
+    Serial.println("Couldn't find MCP9808!");
+    cnt++;
+    if (cnt > 10) {
+      return;
+    }
+    delay(50);
+  }
+  // tempsensor.shutdown_wake(0);
+  // delay(250);
+  tempsensor.setResolution(3);  // 0.0625°C    250 ms
+  float c = tempsensor.readTempC();
+  Serial.println(" MCP9808 temp: " + String(c));
+  Serial.println(" ");
+}    
+
+void init_GPS(void) {
+ // GPS init
+  ss.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin); 
+}
+    
+init_WEB(void) {
+
+  // WEB server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  server.begin();
+}  
+
+void u8g2_ascii_2(void) {
   char s[2] = " ";
   uint8_t x, y;
   u8g2.drawStr( 0, 0, "ASCII page 2");
